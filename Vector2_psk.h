2e14d9ad95bfed9bdc2df70276c9165af820df1a
murmurmew <103969091+murmurmew@users.noreply.github.com>
2023-01-11 11:31:33 +0300

    Update and rename HomeWork4.h to Vector2_psk.h

---
+++ Vector2_psk.h
@@ -0,0 +1,15 @@
+#ifndef DZ_4_MOD_VECTOR2_PSK_H
+#define DZ_4_MOD_VECTOR2_PSK_H
+
+
+
+class Vector2_psk {
+public:
+    double r, phi;
+    Vector2_psk();
+    Vector2_psk(double _r, double _phi);
+
+};
+
+
+#endif //DZ_4_MOD_VECTOR2_PSK_H
