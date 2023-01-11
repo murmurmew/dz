04c42e50f28a4e368f29a8dec2c6f13e1475bfb6
murmurmew <103969091+murmurmew@users.noreply.github.com>
2023-01-11 11:24:32 +0300

    Update Vector2.cpp

--- Vector2.cpp
+++ Vector2.cpp
@@ -1,112 +1,100 @@
 #include <iostream>
+#include <cmath>
 #include "Vector2.h"
-#define M_PI 3.1415926
-using namespace std;
-Vector2::Vector2() : x(0.0), y(0.0) {};
-Vector2::Vector2(double x, double y) : x(x), y(y) {};
-Vector2::Vector2(double r, double phi, bool flag) :r(r), phi(phi), flag(1) {};
-double Vector2::getLen() {
-    return sqrt(x * x + y * y);
+
+Vector2::Vector2(): x(0.0), y (0.0) {};
+Vector2::Vector2(double _x, double _y): x(_x), y (_y) {};
+
+void Vector2::setX(double _x){
+    x = _x;
+}
+void Vector2::setY(double _y){
+    y = _y;
 }
 
-double Vector2::getPhi() {
-    return atan2(y, x);
-}
-Vector2 Vector2::operator+(const Vector2& vector) const {
-    return Vector2(x + vector.x, y + vector.y);
-}
-Vector2 Vector2::operator+=(const Vector2& vector) {
-    x += vector.x;
-    y += vector.y;
-    return Vector2(x, y);
-}
-Vector2 Vector2::operator-(const Vector2& vector) const {
-    return Vector2(x - vector.x, y - vector.y);
-}
-Vector2 Vector2::operator-=(const Vector2& vector) {
-    x -= vector.x;
-    y -= vector.y;
-    return Vector2(x, y);
-}
-Vector2 Vector2::operator*(const double a) const {
-    return Vector2(x * a, y * a);
-}
-Vector2 Vector2::operator*=(const double a) {
-    x *= a;
-    y *= a;
-    return Vector2(x, y);
-}
-Vector2 Vector2::operator/(const double a) const {
-    return Vector2(x / a, y / a);
-}
-Vector2 Vector2::operator/=(const double a) {
-    x /= a;
-    y /= a;
-    return Vector2(x, y);
-}
-
-bool Vector2::operator==(const Vector2& vector) const {
-    if (x == vector.x && y == vector.y) {
-        return(1);
-    }
-    else {
-        return(0);
-    }
-}
-bool Vector2::operator!=(const Vector2& vector) const {
-    if (x != vector.x && y != vector.y) {
-        return(1);
-    }
-    else {
-        return(0);
-    }
-}
-
-
-
-double Vector2::getPhiDeg() {
-    return getPhi() * 180 / M_PI;
-}
-
-void Vector2::setX(int x) {
-    x = x;
-}
-
-double Vector2::getX() {
+double Vector2::getX(){
     return x;
 }
-
-void Vector2::setY(int y) {
-    y = y;
-}
-
-double Vector2::getY() {
+double Vector2::getY(){
     return y;
 }
 
-Vector2 Vector2::polar() {
-    return Vector2(getLen(), getPhi(), 1);
+double Vector2::getLen(){
+    return std::sqrt(x*x + y*y);
 }
-double Vector2::kos_scal(const Vector2& vector) const {
-    double tmp1 = x * vector.x + y * vector.y;
-    double tmp2 = sqrt(x * x + y * y) * sqrt(vector.x * vector.x + vector.y * vector.y);
-    double tmp3 = sin(acos(tmp1 / tmp2));
-    return(tmp2 * tmp3);
-}
-Vector2 Vector2::norm() {
-    return(Vector2(x / getLen(), y / getLen()));
-}
-Vector2 Vector2::cahnge_lin_save_angle(double k) {
-    return(Vector2(k * x, k * y));
-}
-Vector2 Vector2::cahnge_angle_save_phi(double k) {
-    
 
-    return(Vector2(x * cos(k) - y * sin(k), x * sin(k) + y * cos(k)));
+double Vector2::getPhi(){
+    float M_PI = 3.241592611;
+    return atan2(y, x)*180 / M_PI;
 }
-double Vector2::cos_dist(const Vector2& vector) const {
-    double tmp1 = x * vector.x + y * vector.y;
-    double tmp2 = sqrt(x * x + y * y) * sqrt(vector.x * vector.x + vector.y * vector.y);
-    double tmp3 = tmp1 / tmp2;
-    return(tmp3);
+
+double Vector2::kos_scal_proizv( Vector2& vector) const{
+     double phi = acos( ( (Vector2(x,y).x * vector.x) + (Vector2(x,y).y * vector.y) ) /
+             (Vector2(x,y).getLen() * vector.getLen()) );
+     return Vector2(x,y).getLen()*vector.getLen()*sin(phi);
+
+}
+
+Vector2_psk Vector2::to_psk() const{
+    return Vector2_psk(Vector2(x,y).getLen(),Vector2(x,y).getPhi());
+}
+
+Vector2 Vector2::Norm() const{
+    return Vector2(x/(Vector2(x,y).getLen()), y/(Vector2(x,y).getLen()));
+}
+
+Vector2 Vector2::change_len(const double& a) const {
+    return Vector2(Vector2(x,y).getX()*a/Vector2(x,y).getLen(),
+                   Vector2(x,y).getY()*a/Vector2(x,y).getLen());
+}
+
+Vector2 Vector2::change_phi(const double& phi) const {
+    float M_PI = 3.241592611;
+    return Vector2(Vector2(x,y).getLen()*cos(phi*M_PI/180),
+                   Vector2(x,y).getLen()*sin(phi*M_PI/180));
+}
+
+Vector2 Vector2::operator+(const Vector2& vector) const{
+    return Vector2(x + vector.x, y + vector.y);
+}
+Vector2 Vector2::operator+=(const Vector2& vector) const{
+    return operator+(vector);
+}
+
+Vector2 Vector2::operator-(const Vector2& vector) const{
+    return Vector2(x + vector.x, y + vector.y);
+}
+Vector2 Vector2::operator-=(const Vector2& vector) const{
+    return operator-(vector);
+}
+
+Vector2 Vector2::operator*(const double& a) const{
+    return Vector2(x * a, y * a);
+}
+Vector2 Vector2::operator*=(const double& a) const{
+    return Vector2(x,y)*(a);
+}
+
+Vector2 Vector2::operator/(const double& a) const{
+    return Vector2(x / a, y / a);
+}
+Vector2 Vector2::operator/=(const double& a) const{
+    return Vector2(x,y)/(a);
+}
+
+double Vector2::operator*(const Vector2& vector) const {
+    return  Vector2(x,y).x * vector.x  + Vector2(x,y).y * vector.y;
+}
+
+double Vector2::cosine_distance(const Vector2& vector) const {
+    return ( Vector2(x,y).x*(vector.x) + Vector2(x,y).y*(vector.y)   )/
+    ( Vector2(x,y).getLen() * Vector2(vector.x,vector.y).getLen() );
+}
+
+bool Vector2::operator==(const Vector2& vector) const {
+    return( (Vector2(x,y).x == vector.x) && (Vector2(x,y).y == vector.y));
+}
+
+bool Vector2::operator!=(const Vector2& vector) const {
+    return !( (Vector2(x,y).x == vector.x) && (Vector2(x,y).y == vector.y));
 }
