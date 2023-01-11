c5f5f8fd00fa0953ed714a825509d2a9dd7a048d
murmurmew <103969091+murmurmew@users.noreply.github.com>
2023-01-11 11:23:38 +0300

    Update Vector2.h

--- Vector2.h
+++ Vector2.h
@@ -1,41 +1,43 @@
-﻿#ifndef VEC_CPP
-#define VEC_CPP
-#include <cmath>
+#ifndef VECTOR2_H
+#define VECTOR2_H
+#include "Vector2_psk.h"
+
+
 class Vector2 {
 public:
-    double x, y, r, phi;
-    bool flag;
+    double x, y;
+
     Vector2();
-    Vector2(double x, double y);
-    Vector2(double x, double y, bool flag);
+    Vector2(double _x, double _y);
+
     double getLen();
 
-    void setX(int x);
+    void setX(double _x);
     double getX();
-    void setY(int y);
+    void setY(double _y);
     double getY();
 
     double getPhi();
-   
+    double kos_scal_proizv( Vector2& vector) const;
+    Vector2_psk to_psk() const;
+
+    Vector2 Norm() const;
+    Vector2 change_len(const double& a) const;
     Vector2 operator+(const Vector2& vector) const;
+    Vector2 operator+=(const Vector2& vector) const;
     Vector2 operator-(const Vector2& vector) const;
-    Vector2 operator+=(const Vector2& vector);
-    Vector2 operator-=(const Vector2& vector);
-    Vector2 operator*(const double a) const;
-    Vector2 operator/(const double a) const;
-    Vector2 operator*=(const double a);
-    Vector2 operator/=(const double a);
+    Vector2 operator-=(const Vector2& vector) const;
+    Vector2 operator*(const double & a) const;
+    double operator*(const Vector2& vector) const;
+    Vector2 operator*=(const double & a) const;
+    Vector2 operator/(const double & a) const;
+    Vector2 operator/=(const double & a) const;
     bool operator==(const Vector2& vector) const;
     bool operator!=(const Vector2& vector) const;
-
-    //Vector2 operator-=(const Vector2& vector) const;
-
-    double getPhiDeg();
-    Vector2 polar();
-    double kos_scal(const Vector2& vector) const;
-    Vector2 norm();
-    Vector2 cahnge_lin_save_angle(double k);
-    Vector2 cahnge_angle_save_phi(double k);
-    double cos_dist(const Vector2& vector) const;
+    Vector2 change_phi(const double& phi) const;
+    double cosine_distance(const Vector2& vector) const;
 };
-#endif
+
+
+
+#endif //VECTOR2_H
